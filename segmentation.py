de3cd182df53e5931f673ed8515ba4b0756f946b
sustianovich <jmsustacha@gmail.com>
2023-09-26 16:40:48 +0200

    second

--- segmentation.py
+++ segmentation.py
@@ -1,6 +1,4 @@
 import os
-import nibabel as nb
-import matplotlib.pyplot as plt
 
 from nilearn import plotting
 from nipype import Node, Workflow
@@ -17,9 +15,15 @@ from nipype.algorithms.confounds import TSNR
 from nipype import SelectFiles
 from IPython.display import Image
 
+from utils_tools import find_files
+
+words_to_search = ['t1w', 'brain']
+paths_to_files = find_files(words_to_search)
+
 home_directory = os.path.expanduser("~")
 
-in_file = 'data/ADNI/AD/sub-1_ses-timepoint1_run-1_T1w.nii.gz'
+in_file = paths_to_files[0]
+
 preproc = Workflow(name='work_preproc', base_dir=os.path.abspath(in_file))
 
 extract = Node(ExtractROI(t_min=4, t_size=-1, output_type='NIFTI'), name="extract")
